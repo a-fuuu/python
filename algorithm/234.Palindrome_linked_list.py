d9024a315cf741a79e39516330e350294a6fb9b6
a-fuuu <vlvk22tpzjs@gmail.com>
2023-06-25 23:36:02 +0900

    234.Palindrome linked list added

---
+++ algorithm/234.Palindrome_linked_list.py
@@ -0,0 +1,12 @@
+'''
+연결리스트가 팰린드롬인지 판별하시오
+'''
+
+def linked_palindrome(self, )->bool:
+    list = []
+    
+    if not list:
+        return True
+    
+    while input() is not None:
+        list.append
