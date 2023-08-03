466e74cadd5bc0ae7a506bb920986eabad71a453
a-fuuu <vlvk22tpzjs@gmail.com>
2023-08-03 23:06:13 +0900

    merge k sorted list added

---
+++ algorithm/23.Merge_K_Sorted_List.py
@@ -0,0 +1,13 @@
+'''
+k개의 정렬된 리스를 하나의 정렬된 리스트로 병합하라
+'''
+import heapq
+
+class Solution:
+    def __init__(self):
+        self.priority_queue = []
+
+    def merge_sorted_list(self, klist:list)->list:
+        for _ in klist:
+            for _ in
+        
