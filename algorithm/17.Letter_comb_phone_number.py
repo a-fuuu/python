fb24bcf0a11b80178b99ee40c1a5932ff0fb8d5d
a-fuuu <vlvk22tpzjs@gmail.com>
2023-08-11 00:16:43 +0900

    letter comb phone number

---
+++ algorithm/17.Letter_comb_phone_number.py
@@ -0,0 +1,9 @@
+
+class Solution:
+    def letter_comb_phone_number(self, nums:str):
+
+        dict = {"2" : "abc", "3" : "def", "4" : "ghi",
+                "5" : "jkl", "6" : "mno", "7" : "pqrs",
+                "8" : "tuv", "9" : "wxyz"}
+        
+        
