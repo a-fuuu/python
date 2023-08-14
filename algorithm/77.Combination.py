0f21cadb595cff896d269e991044a63867220f1b
a-fuuu <vlvk22tpzjs@gmail.com>
2023-08-14 18:02:56 +0900

    77.combination solution modified

--- algorithm/77.Combination.py
+++ algorithm/77.Combination.py
@@ -12,25 +12,43 @@ n = 4 k = 2
 '''
 
 class Solution:
-    def combination(self, n:int, k:int):
-        def dfs(picked:list[int], nums:list[int]):
-            for num in nums:
-                if len(picked) == k:
-                    return picked
-                else:
-                    picked.append(num)
-                    dfs(picked, nums.remove(num))
+    # def combination(self, n:int, k:int):
+    #     def dfs(picked:list[int], nums:list[int]):
+    #         for num in nums:
+    #             if len(picked) == k:
+    #                 return picked
+    #             else:
+    #                 picked.append(num)
+    #                 dfs(picked, nums.remove(num))
 
-        candi = list(range(1, n+1))
+    #     candi = list(range(1, n+1))
 
+    #     result = []
+
+    #     result.append(dfs([candi.pop()], candi))
+
+    #     return result
+
+    def combine(self, n:int, k:int)->list[list[int]]:
         result = []
 
-        result.append(dfs([candi.pop()], candi))
+        def dfs(elements, start:int, k:int):
+            if k == 0:
+                result.append(elements[:])
+                return
+            
+            for i in range(start, n + 1):
+                elements.append(i)
+                dfs(elements, i + 1, k -1)
+                elements.pop()
+
+        dfs([], 1, k)
 
         return result
+
     
 
 if __name__ == '__main__':
     sol = Solution()
-    res = sol.combination(4, 2)
+    res = sol.combine(5, 3)
     print(res)
