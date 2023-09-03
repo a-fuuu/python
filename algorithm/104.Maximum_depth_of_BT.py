fc48d6908f5ef3d803ebac445474f4dff4604eae
a-fuuu <vlvk22tpzjs@gmail.com>
2023-09-03 23:48:57 +0900

    leetcod 104 complete

--- algorithm/104.Maximum_depth_of_BT.py
+++ algorithm/104.Maximum_depth_of_BT.py
@@ -1,7 +1,12 @@
 import collections
 
+class TreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
 class Solution:
-    def max_dep(self, root: Treenode)->int:
+    def max_dep(self, root: TreeNode)->int:
         if root is None:
             return 0
         queue = collections.deque([root])
@@ -12,6 +17,20 @@ class Solution:
 
             for _ in range(len(queue)):
                 cur_root = queue.popleft()
-
-        
+                if cur_root.left:
+                    queue.append(cur_root.left)
+                if cur_root.right:
+                    queue.append(cur_root.right)
         return depth
+    
+if __name__ == '__main__':
+    sol = Solution()
+    root = TreeNode(3)
+    root.left = TreeNode(9)
+    root.right = TreeNode(20)
+    root.left.left = TreeNode(None)
+    root.left.right = TreeNode(None)
+    root.right.left = TreeNode(15)
+    root.right.right = TreeNode(7)
+    res = sol.max_dep(root)
+    print(res)
