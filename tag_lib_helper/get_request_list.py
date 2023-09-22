0c9df5809ccf1e346fe56fdebdb9e8abc4ccc9f7
a-fuuu <vlvk22tpzjs@gmail.com>
2023-09-22 16:33:59 +0900

    excel 동기화 및 업데이트 기능  구현

--- tag_lib_helper/get_request_list.py
+++ tag_lib_helper/get_request_list.py
@@ -1,56 +1,59 @@
-# import pandas as pd
-# import requests
+import os
+import shutil
+import pandas as pd
+import openpyxl
+
+# 파일의 경로를 지정합니다
+path = "/mnt/c/Users/전준표/OneDrive - 하이퍼라운지/Shared Documents/General/☀︎ 팀 회의 자료/DE_데이터팀/400. Tag Library/통합 문서.xlsx"
+
+filename = os.path.basename(path)
+
+local_path = "./"
+
+# 최신 tag library 요청 리스트를 받기 위해 파일이 존재하는 경우 삭제
+if os.path.exists("./통합 문서.xlsx"):
+    os.remove("./통합 문서.xlsx")
+
+# 파일을 local path에 복사합니다. Onedrive 경로의 파일은 접근 권한 이슈가 있음
+shutil.copyfile(path, os.path.join(local_path, filename))
 
 
 
-# local_file_path = "/home/junpyo/file.xlsx"
 
-# response = requests.get(file_url)
+##################################여기서부터#################################
 
-# if response.status_code == 200:
-#     with open(local_file_path, 'wb') as file:
-#         file.write(response.content)
-#     print(f"File downloaded successfully to {local_file_path}")
-# else:
-#     print(f"Failed to download the file. Status code: {response.status_code}")
+excel_file_path = "./통합 문서.xlsx"
 
+# Excel 파일을 데이터프레임으로 읽어오기
+df = pd.read_excel(excel_file_path, header=1)
 
-import requests
-from requests.auth import HTTPBasicAuth
+# 처리상태가 비어있는 항목들 선택
+todo = df[df['처리상태'].isna()]
 
-# Define your SharePoint site URL
+todo = todo[['처리상태', '내용']]
 
+# 데이터프레임 출력 (옵션)
+# print(todo)
+for row in todo['내용']:
+    print(row)
 
-# Define your client ID and client secret
+# Load the Excel workbook
+workbook = openpyxl.load_workbook("./통합 문서.xlsx")
 
-# Define your SharePoint resource URL (e.g., a file or endpoint you want to access)
+# Select the worksheet you want to modify
+worksheet = workbook['Sheet1']
 
+cell = worksheet.cell(row=2, column=3)  # Row 2, Column C
 
-# Acquire an OAuth2 token using the client ID and client secret
-token_url = f"{sharepoint_site_url}/_layouts/15/appstsserver.aspx?SecurityTokenServiceMachine=your-sts-server"
-token_data = {
-    "grant_type": "client_credentials",
-    "client_id": client_id,
-    "client_secret": client_secret,
-    "resource": sharepoint_resource_url
-}
-token_response = requests.post(token_url, data=token_data)
-print(token_response.text)
-if token_response.status_code == 200:
-    access_token = token_response.json().get("access_token")
+# Change the value of the cell
+cell.value = 'TEST'
 
-    # Include the access token in the request headers
-    headers = {
-        "Authorization": f"Bearer {access_token}"
-    }
+# Save the modified workbook
+workbook.save('통합 문서.xlsx')
 
-    # Make a request to access the SharePoint resource
-    response = requests.get(sharepoint_resource_url, headers=headers)
+path = "/mnt/c/Users/전준표/OneDrive - 하이퍼라운지/Shared Documents/General/☀︎ 팀 회의 자료/DE_데이터팀/400. Tag Library/"
+filename = os.path.basename(excel_file_path)
+if os.path.exists("/mnt/c/Users/전준표/OneDrive - 하이퍼라운지/Shared Documents/General/☀︎ 팀 회의 자료/DE_데이터팀/400. Tag Library/통합 문서.xlsx"):
+    os.remove("/mnt/c/Users/전준표/OneDrive - 하이퍼라운지/Shared Documents/General/☀︎ 팀 회의 자료/DE_데이터팀/400. Tag Library/통합 문서.xlsx")
 
-    if response.status_code == 200:
-        # Process the SharePoint resource here
-        print("Request successful")
-    else:
-        print(f"Failed to access the SharePoint resource. Status code: {response.status_code}")
-else:
-    print(f"Failed to acquire the OAuth2 token. Status code: {token_response.status_code}")
+shutil.copyfile(excel_file_path, os.path.join(path, filename))
