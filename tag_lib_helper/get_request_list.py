c111a97928c69eccb55f446383da7da79cc76f79
a-fuuu <vlvk22tpzjs@gmail.com>
2023-10-23 17:45:41 +0900

    tag lib 자동화 version up 기능 추가

--- tag_lib_helper/get_request_list.py
+++ tag_lib_helper/get_request_list.py
@@ -20,7 +20,6 @@ def file_download():
     # 최신 tag library 요청 리스트를 받기 위해 파일이 존재하는 경우 삭제
     if os.path.exists("./통합 문서.xlsx"):
         os.remove("./통합 문서.xlsx")
-
     # 파일을 local path에 복사합니다. Onedrive 경로의 파일은 접근 권한 이슈가 있음
     shutil.copyfile(path, os.path.join(local_path, filename))
 
@@ -30,6 +29,7 @@ def translate(text):
     한-영 번역을 수행하는 함수입니다.
     '''
 
+    # API key
     with open('./key.txt', 'r') as key_file:
         contents = key_file.read()
         contents = contents.split('\n')
@@ -59,11 +59,23 @@ def extract_translate_append():
     wb = openpyxl.load_workbook(excel_file_path)
     sheet = wb['Sheet1']
 
-    file_path = './hl_standards_new_tag.json'
+    file_path = '../../../professional/modelmapping/de_server/db_to_excel/hl_standards_new_tag.json'
 
     with open(file_path, encoding='utf-8') as f:
         data = json.load(f)
     
+    version = data["version"]
+    version = version.split(".")
+    version = [int(x) for x in version]
+    version[-1] = version[-1] + 1
+    if version[-1] == 100:
+        version[-2] = version[-2] + 1
+        version[-1] = 0
+    version = [str(x) for x in version]
+    version[-1] = "{:0<{width}}".format(version[-1], width=2)
+    version = ".".join(version)
+
+    data["version"] = version
     result = []
     for row in sheet.iter_rows(min_row=2, max_row=sheet.max_row, min_col=14, max_col=14):
         for cell in row:
@@ -85,15 +97,12 @@ def extract_translate_append():
     for item in result:
         data["hl_standards_tag"][0]["analysis_item"][0]['key_tag'][0]['common'].append(item)
     
-    with open('./hl_standards_new_tag_1.json', 'w', encoding='utf-8') as output:
+    if os.path.exists(f'./hl_standards_new_tag_v{version}.json'):
+        os.remove(f'./hl_standards_new_tag_v{version}.json')
+
+    with open(f'./hl_standards_new_tag_v{version}.json', 'w', encoding='utf-8') as output:
         json.dump(data, output, indent=2, ensure_ascii=False)
     wb.close()
-
-    # path = "/mnt/c/Users/전준표/OneDrive - 하이퍼라운지/Shared Documents/General/☀︎ 팀 회의 자료/DE_데이터팀/400. Tag Library/"
-    # filename = os.path.basename(excel_file_path)
-    # if os.path.exists("/mnt/c/Users/전준표/OneDrive - 하이퍼라운지/Shared Documents/General/☀︎ 팀 회의 자료/DE_데이터팀/400. Tag Library/통합 문서.xlsx"):
-    #     os.remove("/mnt/c/Users/전준표/OneDrive - 하이퍼라운지/Shared Documents/General/☀︎ 팀 회의 자료/DE_데이터팀/400. Tag Library/통합 문서.xlsx")
-    # shutil.copyfile(excel_file_path, os.path.join(path, filename))
 
 if __name__=='__main__':
     # 수행요청 list file 다운로드
